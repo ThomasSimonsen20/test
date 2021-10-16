40208cc7b7de15662f11a24dbdc891fa37410bd5
Kameloozo <64187942+Kameloozo@users.noreply.github.com>
2021-10-16 10:29:36 +0200

    test

---
+++ components/Test2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2>test2</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/main.scss";
+
+h2 {
+    font-size: 2rem;
+    color: aqua;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <Test1/>
+</div>
+</template>
+
+<script>
+import Test2 from '../components/Test2.vue'
+import Test1 from '../components/Test1.vue'
+export default {
+  components: { Test1 },}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/main.scss";
+</style>
